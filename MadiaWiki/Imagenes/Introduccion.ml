8a55282f8aa594e166414930bb1561b264be32af
ElisaaCordoba <elisitacordoba7@gmail.com>
2024-11-28 17:18:31 +0100

    Update Introduccion.ml

--- MadiaWiki/Imagenes/Introduccion.ml
+++ MadiaWiki/Imagenes/Introduccion.ml
@@ -1,5 +1,6 @@
 En esta carpeta se muestran las imagenes de las sentencias de estos comandos:
 Mediawiki1.png: docker run -d -p 8080:80 --name mediawiki1 mediawiki
+[mediawiki1.png](https://github.com/ElisaaCordoba/PracticaDocker/blob/main/MadiaWiki/Imagenes/Mediawiki1.png)
 Mediawiki2.png: docker run -d -p 8081:80 --name mediawiki2 mediawiki:1.36.3
 Mediawikie.png: docker run -d -p 8082:80 --name mediawiki3 mediawiki:1.35.5
 
